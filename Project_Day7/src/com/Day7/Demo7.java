eac124659c1047d8c574ec5a108a3122ebc34380
Indrajit <indraindrani1999@gmail.com>
2022-08-08 03:13:48 +0530

    Added

---
+++ Project_Day7/src/com/Day7/Demo7.java
@@ -0,0 +1,28 @@
+package com.Day7;
+
+import java.util.Scanner;
+
+public class Demo7 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Number in Matrix");
+		
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int n3 = sc.nextInt();
+		
+		sc.nextLine();
+		
+		int n5 = sc.nextInt();
+		int n6 = sc.nextInt();
+		int n7 = sc.nextInt();
+		
+		System.out.println(n1 + " " + n2 + " " + n3);
+		System.out.println(n5 + " " + n6 + " " + n7);
+
+	}
+
+}
